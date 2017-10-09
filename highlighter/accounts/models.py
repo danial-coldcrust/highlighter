b0f17ce09e07037dc54b27f8f03e06d8e891f473
jinwoo <jinwoo4130@naver.com>
2017-10-09 14:49:09 +0900

    accounts.models.Profile 모델생성/admin등록

--- highlighter/accounts/models.py
+++ highlighter/accounts/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
+#회원관리 장고에서 지원해줌 근데 커스터마이징 불가
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.ForeignKey('auth.User') #FIXME:Bad case!!
+    phone_number = models.CharField(max_length=20)
+    address = models.CharField(max_length=50)
