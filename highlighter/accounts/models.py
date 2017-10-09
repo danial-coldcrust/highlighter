da9c9f0a984cf52c807827469cee340765aef1a5
jinwoo <jinwoo4130@naver.com>
2017-10-09 15:41:11 +0900

    settingsAUTH_USER_MODEL 을 안전하게 수정함

--- highlighter/accounts/models.py
+++ highlighter/accounts/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.conf import settings
 #회원관리 장고에서 지원해줌 근데 커스터마이징 불가
 
 class Profile(models.Model):
-    user = models.ForeignKey('auth.User') #FIXME:Bad case!!
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
     phone_number = models.CharField(max_length=20)
     address = models.CharField(max_length=50)
+    credit = models.IntegerField()
