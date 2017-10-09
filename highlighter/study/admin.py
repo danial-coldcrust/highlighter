9b32cbd230b30585df759f75f772299f7d61d312
jinwoo <jinwoo4130@naver.com>
2017-10-09 19:22:40 +0900

    스터디에Post추가

--- highlighter/study/admin.py
+++ highlighter/study/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Study,GroupTodo
+from .models import Study,GroupTodo,Post
 
 @admin.register(Study)
 class CommentAdmin(admin.ModelAdmin):
@@ -9,4 +9,8 @@ class CommentAdmin(admin.ModelAdmin):
 class CommentAdmin(admin.ModelAdmin):
         pass
 
+
+@admin.register(Post)
+class CommentAdmin(admin.ModelAdmin):
+        pass
 
--- highlighter/study/models.py
+++ highlighter/study/models.py
@@ -23,3 +23,21 @@ class GroupTodo(models.Model):
 
     def __str__(self):
         return self.todo
+
+class Post(models.Model):
+
+    STATUS_CHOICES = (
+        ('d','Draft'),
+        ('p', 'Published'),
+    )
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    study = models.ForeignKey(Study)
+    title = models.CharField(max_length=100, verbose_name='제목')
+    content = models.TextField(verbose_name='내용')
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
