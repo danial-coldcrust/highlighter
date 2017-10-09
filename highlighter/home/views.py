bc667b88ba53efb25c674e1a5b2e2afdae471da1
jinwoo <jinwoo4130@naver.com>
2017-10-09 11:13:56 +0900

    간단한검색구현

--- highlighter/home/views.py
+++ highlighter/home/views.py
@@ -4,6 +4,12 @@ from .models import Project
 
 def project_list(request):
     qs = Project.objects.all()
+
+    q = request.GET.get('q','')
+    if q:
+        qs = qs.filter(title__icontains=q)
+
     return render(request, 'project_list.html',{
         'project_list':qs,
+        'q':q,
     })
