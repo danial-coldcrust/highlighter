e3f3a8387d440322589c9d8660e5ced099ed2fa5
jinwoo <jinwoo4130@naver.com>
2017-10-11 22:07:16 +0900

    media 파일 서빙 지원

--- highlighter/highlighter/urls.py
+++ highlighter/highlighter/urls.py
@@ -1,7 +1,9 @@
 from django.conf import settings
 from django.conf.urls import url,include
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.shortcuts import redirect
+
 
 
 urlpatterns = [
@@ -17,3 +19,5 @@ if settings.DEBUG:
     urlpatterns += [
         url(r'^__debug__/',include(debug_toolbar.urls)),
     ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
