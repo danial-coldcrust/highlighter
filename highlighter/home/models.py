c88a066a254556e1d55b2af5d5c1c05634a80b83
jinwoo <jinwoo4130@naver.com>
2017-10-12 01:07:59 +0900

    썸네일 3번째꺼 템플릿으로 하는거 활용함

--- highlighter/home/models.py
+++ highlighter/home/models.py
@@ -23,17 +23,17 @@ class Project(models.Model):
 
     user = models.ForeignKey(settings.AUTH_USER_MODEL)
     title = models.CharField(max_length=100, verbose_name='제목',validators=[min_length_3_validator])
+
     content = models.TextField(verbose_name='내용')
     photo = models.ImageField(blank=True, upload_to='home/project/%Y/%m/%d')
-    photo_thumbnail = ImageSpecField(source='photo',
-                                    processors=[Thumbnail(300,300)],
-                                    format='JPEG',
-                                    options={'quality',60})
+    # photo_thumbnail = ImageSpecField(source='photo', processors=[Thumbnail(300, 300)], format='JPEG',
+    #                                         options={'quality', 60})
     status = models.CharField(max_length=1, choices=STATUS_CHOICES)
     tag_set = models.ManyToManyField('Tag',blank=True)
     #태그는솔찍히빈칸이여도됨빈칸으로하면에러남
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
 
     def __str__(self):
         return self.title
