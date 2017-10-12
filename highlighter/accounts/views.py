7bd10e4f2f34f84ada44de03c62e762013da59fb
jinwoo <jinwoo4130@naver.com>
2017-10-12 10:30:01 +0900

    회원가입 구현

--- highlighter/accounts/views.py
+++ highlighter/accounts/views.py
@@ -1,5 +1,17 @@
-from django.shortcuts import render
+from django.conf import settings
+from django.shortcuts import redirect,render
+from django.contrib.auth.forms import UserCreationForm
 
-
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            return redirect(settings.LOGIN_URL) #default : "/accounts/login/"
+    else:
+        form = UserCreationForm()
+    return render(request, 'accounts/signup_form.html', {
+        'form': form,
+    })
 def profile(request):
     return render(request, 'accounts/profile.html')
