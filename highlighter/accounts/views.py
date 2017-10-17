6ab3f000400bacb826c261fd33426bc10becc311
jinwoo <jinwoo4130@naver.com>
2017-10-17 23:35:56 +0900

    django-allauth를 통해 페북네이버카카오 provider 추가

--- highlighter/accounts/views.py
+++ highlighter/accounts/views.py
@@ -1,7 +1,10 @@
 from django.conf import settings
-from django.shortcuts import redirect,render
 from django.contrib.auth.decorators import login_required
-from .forms import SignupForm
+from django.contrib.auth.views import login as auth_login
+from django.shortcuts import redirect,render
+from allauth.socialaccount.models import SocialApp
+from allauth.socialaccount.templatetags.socialaccount import get_providers
+from .forms import SignupForm,LoginForm
 
 def signup(request):
     if request.method == 'POST':
@@ -20,6 +23,19 @@ def profile(request):
     return render(request, 'accounts/profile.html')
 
 
-# def login(request):
-#     LoginForm
-#     pass
+def login(request):
+    providers = []
+
+    for provider in get_providers():
+
+        try:
+            provider.social_app = SocialApp.objects.get(provider=provider.id, sites=settings.SITE_ID)
+        except SocialApp.DoesNotExist:
+            provider.social_app = None
+        providers.append(provider)
+
+
+    return auth_login(request,
+                      authentication_form=LoginForm,
+                      template_name='accounts/login_form.html',
+                      extra_context={'providers': providers})
