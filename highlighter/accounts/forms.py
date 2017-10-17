1cf0495614b5da2bb44df654497592a1df60d6ea
jinwoo <jinwoo4130@naver.com>
2017-10-17 19:28:05 +0900

    회원가입 시에 이메일 추가 입력

---
+++ highlighter/accounts/forms.py
@@ -0,0 +1,5 @@
+from django.contrib.auth.forms import UserCreationForm
+
+class SignupForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        fields = UserCreationForm.Meta.fields + ('email',)
